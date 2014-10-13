61a9674411d57cfa31274127dacb557705d33b62
Kenichi Takahashi <takahashi.kenichi@gmail.com>
2014-10-13 18:32:50 +0900

    Update Lesson15

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -6,18 +6,32 @@ public class GameController: MonoBehaviour {
 	public GUITexture texClear;
 	public GUITexture texFailed;
 
+	// ステージ終了フラグ
+	private bool isStageEnd;
+
 	// Use this for initialization
 	void Start () {
 		texClear.enabled = false;
 		texFailed.enabled = false;
-	
+		isStageEnd = false;
+	}
+
+	void Update() {
+		// ステージが終了していてマウスの左クリックかタッチパネルが押されたら
+		if (isStageEnd
+			&& (Input.GetKey (KeyCode.Mouse0) || Input.touchCount > 0)) {
+			// シーンMenuをロードする
+			Application.LoadLevel ("Menu");
+		}
 	}
 
 	void StageClear() {
 		texClear.enabled = true;
+		isStageEnd = true;
 	}
 
 	void StageFailed() {
 		texFailed.enabled = true;
+		isStageEnd = true;
 	}
 }
